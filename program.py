4b81233e655a384a6b1444bf987c1b5d30bf99cf
Robert Depweg <rdepweg04@gmail.com>
2024-10-22 17:49:26 -0400

    Add datastructures. Add classes LinkedList and Node. Add removal from front, removal from back, add to front, add to back methods.

--- program.py
+++ program.py
@@ -1,6 +1,15 @@
 """Program code"""
 
+# First-party imports
+from datastructures import LinkedList
+
 
 def main(*args):
     """Method to run program"""
-    print("Hello World!")
+    # Make instance of Linked List.
+    linked_list = LinkedList()
+
+    # Add to the front and back a few times.
+    linked_list.add_to_front(5)
+    linked_list.add_to_front(3)
+    linked_list.add_to_front(1)
